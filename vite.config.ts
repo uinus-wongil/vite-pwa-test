c9bbe8614ed816e244fe89bd5959ce56f1125654
asp5091 <asp5091@naver.com>
2024-10-21 16:47:01 +0900

    ref: set icon name

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,37 +10,38 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
-        name: "testApp",
-        short_name: "testApp",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#FFFFFF",
-        theme_color: "#FFFFFF",
-        description: "PWA test",
+        name: 'testApp',
+        short_name: 'testApp',
+        start_url: '/',
+        display: 'fullscreen',
+        background_color: '#FFFFFF',
+        theme_color: '#FFFFFF',
+        description: 'PWA test',
         icons: [
           {
-            src: "/favicon.ico",
-            sizes: "64x64 32x32 24x24 16x16",
-            type: "image/x-icon"
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
           },
           {
-            src: "/android-chrome-192x192.png",
-            type: "image/png",
-            sizes: "192x192"
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: "/android-chrome-512x512.png",
-            type: "image/png",
-            sizes: "512x512"
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
           },
           {
-            src: "/android-chrome-512x512.png",
-            type: "image/png",
-            sizes: "512x512",
-            purpose: "any maskable"
-          }
-        ]
-      }
-    })
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
   ],
-})
+});
